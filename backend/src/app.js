352c99b90aaf6e6ace7489d0ebf23cc08134ae8c
eduardoJunior09 <eduardojunior.dev@gmail.com>
2025-06-07 16:51:35 -0300

    feat: conf inicial do banco de dados Sqlite

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,20 @@
-const express = require("express");
+import {openDb} from "./configDB.js";
+import express from "express";
+
 const app = express();
+app.use(express.json());
+
+openDb();
 
 app.get("/", function (req, res) {
   res.send("Deu Certo!");
+});
+
+app.post("/user", (req, res) => {
+  console.log(req.body);
+  res.json({
+    statusCode: 200,
+  });
 });
 
 app.listen(3000, () => console.log("API Rodando"));
